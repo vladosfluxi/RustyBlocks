ab16a141bd1674851a7281c7b07933c6e8c8dd78
vladosfluxi <viperviperiv@gmail.com>
2026-04-10 13:41:48 +0300

    Add serialize function in implementation of the block: src/block.rs

--- src/block.rs
+++ src/block.rs
@@ -1,20 +1,36 @@
+use crate::crypto::double_hash;
 use crate::transaction::Transaction;
 
 pub struct Block {
     pub header: BlockHead,
-    pub body:   BlockBody,
+    pub body: BlockBody,
 }
 
 pub struct BlockHead {
-    pub index:            u32,
+    pub index: u32,
     pub merkle_root_hash: [u8; 32],
-    pub hash:             [u8; 32],
-    pub hash_prev:        [u8; 32],
-    pub difficulty:       u8,
-    pub timestamp:        u64,
-    pub nonce:            u64,
+    pub hash: [u8; 32],
+    pub hash_prev: [u8; 32],
+    pub difficulty: u8,
+    pub timestamp: u64,
+    pub nonce: u64,
 }
 
 pub struct BlockBody {
     pub transactions: Vec<Transaction>,
+}
+
+impl BlockHead {
+    pub fn serialize(&self) -> [u8; 32] {
+        let mut unhashed = Vec::new();
+
+        unhashed.extend_from_slice(&self.index.to_le_bytes());
+        unhashed.extend_from_slice(&self.merkle_root_hash);
+        unhashed.extend_from_slice(&self.hash_prev);
+        unhashed.extend_from_slice(&self.difficulty.to_le_bytes());
+        unhashed.extend_from_slice(&self.timestamp.to_le_bytes());
+        unhashed.extend_from_slice(&self.nonce.to_le_bytes());
+
+        double_hash(&unhashed)
+    }
 }
